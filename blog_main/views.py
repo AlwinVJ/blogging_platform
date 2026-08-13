727ad61f4c0329fd5781187382fa567223d04fcf
AlwinVJ <alwinvj5@gmail.com>
2026-08-13 21:41:20 +0530

    docs: understand blog foreign key relationships

--- blog_main/views.py
+++ blog_main/views.py
@@ -5,9 +5,11 @@ from blogs.models import Blog, Category
 
 def home(request):
     categories = Category.objects.all()
-    featured_blogs = Blog.objects.filter(is_featured = True).order_by('-updated_at')
+    featured_blogs = Blog.objects.filter(is_featured = True, status = "Published").order_by('-updated_at')
+    not_featured_blogs = Blog.objects.filter(is_featured = False, status = "Published").order_by('-created_at')
     context = {
         'categories':categories,
-        'featured_blogs':featured_blogs
+        'featured_blogs':featured_blogs,
+        'not_featured_blogs':not_featured_blogs
     }
     return render(request, 'home.html',context)

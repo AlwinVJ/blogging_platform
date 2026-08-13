93ace9ecfe6eada350388c0453dc6591d6c6f41d
AlwinVJ <alwinvj5@gmail.com>
2026-08-13 22:45:04 +0530

    feat:handling invalid category requests

--- blogs/views.py
+++ blogs/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
-from blogs.models import Blog
+from django.shortcuts import get_object_or_404, redirect, render
+from blogs.models import Blog, Category
 
 # Create your views here.
 
 def posts_by_category(request, category_id):
     posts = Blog.objects.filter(status="Published",category = category_id)
+    try:
+        category = Category.objects.get(id = category_id)
+    except:
+        return redirect('home.html')
+    # category = get_object_or_404(Category,id=category_id)
+    categories = Category.objects.all()
     context = {
-        'posts':posts
+        'posts':posts,
+        'category_id':category_id,
+        'category':category,
+        'categories':categories
     }
     
     return render(request,'post_by_category.html',context)

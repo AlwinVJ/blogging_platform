825476120adbfd5145485559938c6ced3259267b
AlwinVJ <alwinvj5@gmail.com>
2026-08-14 01:11:33 +0530

    feat: added about section

---
+++ about_social/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AboutSocialConfig(AppConfig):
+    name = 'about_social'
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from about_social.models import About
 from blogs.models import Blog, Category
 
 
@@ -7,9 +7,15 @@ def home(request):
     categories = Category.objects.all()
     featured_blogs = Blog.objects.filter(is_featured = True, status = "Published").order_by('-updated_at')
     not_featured_blogs = Blog.objects.filter(is_featured = False, status = "Published").order_by('-created_at')
+    
+    try:
+        about = About.objects.get()
+    except:
+        about = None
     context = {
         'categories':categories,
         'featured_blogs':featured_blogs,
-        'not_featured_blogs':not_featured_blogs
+        'not_featured_blogs':not_featured_blogs,
+        'about':about
     }
     return render(request, 'home.html',context)

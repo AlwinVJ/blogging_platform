7fdb8be9da42ffae8132eba0c3958bbc89218b6d
AlwinVJ <alwinvj5@gmail.com>
2026-08-14 01:59:55 +0530

    feat: Implemented the search functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('category/',include('blogs.urls')),
     path('<slug:slug>/', BlogView.blogs, name='blogs'),
+    path('blogs/search/',BlogView.search,name='search'),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from blogs.models import Blog, Category
+from django.db.models import Q
 
 # Create your views here.
 
@@ -26,3 +27,8 @@ def blogs(request, slug):
         'single_blog':single_blog
     }
     return render(request,'blogs.html',context)
+
+def search(request):
+    keyword = request.GET.get('keyword')
+    blogs = Blog.objects.filter(Q(title__icontains = keyword) | Q(short_description__icontains = keyword) | Q(blog_body = keyword), status = "Published")
+    return render(request, 'search.html')

bd9070b06ec3fb2ffc34581e279aa7277f16be2b
AlwinVJ <alwinvj5@gmail.com>
2026-08-14 02:11:50 +0530

    feat: Implemeted search layout and corrected the errors

--- blogs/views.py
+++ blogs/views.py
@@ -31,4 +31,8 @@ def blogs(request, slug):
 def search(request):
     keyword = request.GET.get('keyword')
     blogs = Blog.objects.filter(Q(title__icontains = keyword) | Q(short_description__icontains = keyword) | Q(blog_body = keyword), status = "Published")
-    return render(request, 'search.html')
+    context = {
+        'blogs':blogs,
+        'keyword':keyword,
+    }
+    return render(request, 'search.html', context)

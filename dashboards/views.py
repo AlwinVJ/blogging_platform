7dce522acc26544e032e2afed983bb2756b8d180
AlwinVJ <alwinvj5@gmail.com>
2026-08-14 07:15:43 +0530

    refactor: Added login_requiered decorator to dashboard view

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from blogs.models import Blog, Category
 
 # Create your views here.
+
+@login_required(login_url='login')
 def dashboard(request):
     category_count = Category.objects.all().count()
     blogs_count = Blog.objects.all().count()

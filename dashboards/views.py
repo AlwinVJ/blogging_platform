57f32c1a0d431b75cefe7015a8ea4f3323dddd1b
AlwinVJ <alwinvj5@gmail.com>
2026-08-14 07:47:38 +0530

    feat: Implemented add category functionality

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from blogs.models import Blog, Category
 from .forms import CategoryForm
@@ -20,6 +20,11 @@ def categories(request):
     return render(request,'dashboards/categories.html')
 
 def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
     form = CategoryForm()
     context = {
         'form':form

0a606bf43cff2f94d46ba414606c8ad78d587f7e
AlwinVJ <alwinvj5@gmail.com>
2026-08-15 19:40:05 +0530

    feat: Implemented post editing feature

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('categories/delete/<int:pk>',views.delete_category, name = 'delete_category'),
     #Blog post CRUD
     path('posts/',views.posts,name='posts'),
-    path('posts/add/',views.add_post,name='add_post')
+    path('posts/add/',views.add_post,name='add_post'),
+    path('posts/edit/<int:pk>',views.edit_post, name = 'edit_post'),
 ]

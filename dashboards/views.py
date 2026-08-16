3fb72f4926a5c1f73c20ff9d5069698102ac180e
AlwinVJ <alwinvj5@gmail.com>
2026-08-16 10:21:00 +0530

    feat:Implemented add user functionality

--- dashboards/views.py
+++ dashboards/views.py
@@ -108,6 +108,11 @@ def users(request):
     return render(request,'dashboards/users.html',context)
 
 def add_user(request):
+    if request.method == 'POST':
+        form = AddUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
     form = AddUserForm()
     context = {
         'form':form
